85219e753b09944031d6993f04d80c49f360d47a
dev-jv <dev.javierleon@gmail.com>
2020-11-10 18:55:50 -0500

    four

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -2,7 +2,7 @@
 
 
 // Cuando se crea un nuevo cliente
-exports.nuevocliente = (req, res, next) => {
+exports.nuevoCliente = (req, res, next) => {
     // TODO : Insertar en la base de datos
 
     res.json({ mensaje : 'El cliente de agregó correctamente'});
--- API/index.js
+++ API/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 // crear el servidor
 const app = express();
@@ -10,7 +11,10 @@ mongoose.connect('mongodb://localhost/veterinaria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
+});
+
+// Habliltar routing - middlewares express
+app.use('/', routes())
 
 // puerto y arrancar el servidor
 app.listen(4000, () => {
