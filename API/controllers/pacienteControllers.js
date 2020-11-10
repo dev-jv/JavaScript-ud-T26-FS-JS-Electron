f03e9669330d3a150aa333a7d3d8c72fb9f05a1a
dev-jv <dev.javierleon@gmail.com>
2020-11-10 18:36:15 -0500

    three

---
+++ API/controllers/pacienteControllers.js
@@ -0,0 +1,12 @@
+
+
+
+// Cuando se crea un nuevo cliente
+exports.nuevocliente = (req, res, next) => {
+    // TODO : Insertar en la base de datos
+
+    res.json({ mensaje : 'El cliente de agregó correctamente'});
+}
+
+
+
