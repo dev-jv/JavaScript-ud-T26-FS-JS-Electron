3cc7973a671f8124859a97bb4879458ef4ed28db
dev-jv <dev.javierleon@gmail.com>
2020-11-10 23:39:28 -0500

    five

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -4,6 +4,7 @@
 // Cuando se crea un nuevo cliente
 exports.nuevoCliente = (req, res, next) => {
     // TODO : Insertar en la base de datos
+    console.log(req.body);
 
     res.json({ mensaje : 'El cliente de agregó correctamente'});
 }
--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 // crear el servidor
 const app = express();
@@ -12,6 +13,10 @@ mongoose.connect('mongodb://localhost/veterinaria', {
     useUnifiedTopology: true,
     useFindAndModify: false
 });
+
+// Habilitar el body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Habliltar routing - middlewares express
 app.use('/', routes())
