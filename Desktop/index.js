405e69f810062365cc32fd9b14fe35297769a7bf
dev-jv <dev.javierleon@gmail.com>
2020-11-14 00:26:00 -0500

    twenty-seven

---
+++ Desktop/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+let appWindow;
+
+function crearVentana() {
+    appWindow = new BrowserWindow({
+
+    });
+
+    // Cuando la aplicacion es cerrada
+    appWindow.on('closed', () => {
+        appWindow = null;
+    });
+
+    // Cargar HTML
+    appWindow.loadFile('./index.html')
+
+    // Cuando la app este lista, mostrar la ventana
+    appWindow.once('ready-to-show', () => {
+        appWindow.show();
+    });
+}
+
+app.on('ready', crearVentana);
+
+
+
+
+
+
